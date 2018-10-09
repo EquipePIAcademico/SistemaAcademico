8cc2635d0f38041c5bdf040604cfec2c70a5ca2a
iago-sc <Usuario@Acer.tubarao.ifsc.edu.br>
2018-10-09 14:08:32 -0300

    Teste de alteração 2

--- SistemaAcademico/cabecalho.php
+++ SistemaAcademico/cabecalho.php
@@ -1,11 +1,16 @@
+<?php
+                require_once 'usuario/autenticacao.php';
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
         <title>Home Page</title>
-        <link href="css/estilo.css" rel="stylesheet">
+       <link href="css/estilo.css" rel="stylesheet">
 
         <style>
+            
             .dropbtn {
                 background-color: #80d695;
                 color: white;
@@ -28,6 +33,7 @@
                 box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
                 z-index: 1;
             }
+
 
             .dropdown-matricula {
                 display: none;
@@ -60,6 +66,13 @@
                 padding: 12px 16px;
                 text-decoration: none;
                 display: block;
+            }
+            .dropdown-admin a {
+              
+                padding: 12px 16px;
+                text-decoration: none;
+                display: inline;
+                 background-color: #3e8e41;
             }
 
             .dropdown-gerenciamento a {
@@ -95,31 +108,44 @@
             justify-content: center;
             }
             .login{
-                 display: flex;
+            display: flex;
             justify-content: center;
             }
+            .l{
+                margin-top: 50px;
+                margin-left: -120px;
+            }
+            
             
         </style>
     </head>
     <body>
 
         <header id="cabecalho">
-            <div class="dropdown">
+            
                 <!--  <ul type="disc">-->
                 <?php
-                require_once 'usuario/autenticacao.php';
 
                 if (estaLogado()) {
                     if (exibirUsername() == 'administrador') {
                        // echo '<h3 class="nome">Olá ' . exibirUsername() . '  </h3> ';
-                        echo "<a href='http://localhost/SistemaAcademico/usuario/form_inserir.php'>Cadastrar usuários </a>";
-                        echo "<a href='http://localhost/SistemaAcademico/usuario/listar.php'>Gerenciamento de usuários   </a>";
-                        echo "<a href='http://localhost/SistemaAcademico/usuario/listar_perfil.php'>Meu perfil   </a>";
+                         ?>
+                           <div class="dropdown"><?php
+                        echo "<a href='http://localhost/SistemaAcademico/usuario/form_inserir.php'><button class='dropbtn'>Cadastrar usuários </button></a>";
+                       ?> </div>
+                      <div class="dropdown"><?php 
+                       echo "<a href='http://localhost/SistemaAcademico/usuario/listar.php'><button class='dropbtn'>Gerenciamento de usuários</button></a>";
+                     ?></div>
+                     <div class="dropdown"><?php 
+                       echo "<a href='http://localhost/SistemaAcademico/usuario/listar_perfil.php'><button class='dropbtn'>Meu perfil </button></a>";
+                       ?></div>
+                   <?php
                     } else if (exibirPerfilAcesso() == 'secretario(a)') {
                       //  echo '<h3 class="nome">Olá ' . exibirUsername() . '  </h3> ';
+                     ?>   <div class="dropdown"><?php 
                         echo "<a href=//localhost/SistemaAcademico/index.php><button class='dropbtn'>Home</button> </a>   ";
                         ?>
-                    </div>
+                     </div>
                     <div class="dropdown">
                         <?php echo "<button class='dropbtn'> Cadastros</button>"; ?>
 
@@ -136,19 +162,23 @@
         <?php echo "<button class='dropbtn'>Matriculas</button>"; ?>
                         <div class="dropdown-matricula"><?php
                         echo "<a href='http://localhost/SistemaAcademico/matricula_aluno_curso/form_inserir.php'>Matricular aluno em curso</a>";
-                        echo "<a href='http://localhost/SistemaAcademico/matricula_aluno_curso/listar_curso.php'>Alunos Matriculados em Curso</a>";
+                      
                         echo "<a href='http://localhost/SistemaAcademico/matricula_aluno_turma/form_inserir.php'>Matricular aluno em turma</a>";
-                        echo "<a href='http://localhost/SistemaAcademico/matricula_aluno_turma/listar_turmas.php'>Alunos Matriculados em turma</a>";
+                       
                         ?></div>
                     </div>
                     <div class="dropdown">   
                             <?php echo "<button class='dropbtn'>Gerenciamento</button>"; ?>
                         <div class="dropdown-gerenciamento"><?php
-                    echo "<a href='http://localhost/SistemaAcademico/aluno/listar.php'>Gerenciamento de alunos   </a>";
+                  echo "<a href='http://localhost/SistemaAcademico/matricula_aluno_curso/listar_cursos.php'>Alunos Matriculados em Curso</a>";
+                       echo "<a href='http://localhost/SistemaAcademico/matricula_aluno_turma/listar_turmas.php'>Alunos Matriculados em turma</a>";
+                        echo "<a href='http://localhost/SistemaAcademico/aluno/listar.php'>Gerenciamento de alunos   </a>";
                     echo "<a href='http://localhost/SistemaAcademico/tipo/listar.php'>Gerenciamento de tipos de curso   </a>";
                     echo "<a href='http://localhost/SistemaAcademico/curso/listar.php'>Gerenciamento de cursos   </a>";
                     echo "<a href='http://localhost/SistemaAcademico/disciplina/listar.php'>Gerenciamento de disciplinas   </a>";
                     echo "<a href='http://localhost/SistemaAcademico/turma/listar.php'>Gerenciamento de turmas   </a>";
+                      
+                      
                             ?>  </div>
                     </div>
 
@@ -163,15 +193,16 @@
                         ?>
                 <div class="dropdown">
                 <?php
-                echo "<a href='http://localhost/SistemaAcademico/usuario/logout.php'><button class='dropbtn'>Logout</button></a>";
-            } else {
-                echo '<p>Seja Bem-Vindo(a)</p>';
-                ?>
-                    <div class="dropdown login">
-                    <?php
-                    echo "<a href='http://localhost/SistemaAcademico/usuario/form_login.php'><button class='dropbtn'>Login</button>   </a>";
+              echo "<a href='http://localhost/SistemaAcademico/usuario/logout.php'><button class='dropbtn'>Logout</button></a>";
+            ?>
+              </div><?php
+              
+                        } else {
+                
+                header('Location: http://localhost/SistemaAcademico/usuario/form_login.php');
+                 
                 }
-                ?>
+                ?></div>
                     </header>
                     </body>
                     </html>
