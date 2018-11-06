0a96e39b1a1b2282726f80dbc9d6c1d2ce8f75bd
Iago-sc <42946382+Iago-sc@users.noreply.github.com>
2018-11-05 22:44:44 -0200

    Update conectar.php

--- SistemaAcademico/ bd/conectar.php
+++ SistemaAcademico/ bd/conectar.php
@@ -1,2 +1,8 @@
 <?php
-$conexao = mysqli_connect("localhost", "root", "ifsc", "bancoSistema");
+
+//$url = "http://sistemaacademicopi.freevar.com";
+$url = "http://localhost/SistemaAcademico";
+
+//$conexao = mysqli_connect("localhost", "1123917", "naovoudizer", "1123917" , "3306");
+$conexao = mysqli_connect("localhost", "root", "", "bancoSistema");
+
