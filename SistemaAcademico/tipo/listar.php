9b85dcd0c71570e58f9ca531b30d7bb71ea4988b
Iago-sc <42946382+Iago-sc@users.noreply.github.com>
2018-11-05 22:57:12 -0200

    Update listar.php

--- SistemaAcademico/tipo/listar.php
+++ SistemaAcademico/tipo/listar.php
@@ -30,125 +30,117 @@
 
             }
 
-            #customers th {
-                padding-top: 12px;
-                padding-bottom: 12px;
-                text-align: left;
-                background-color: #4CAF50;
-                color: white;
-            }
-
-            body{
-                background-color: #dddddd;
-                color: rgba(0,0,0,1);
-                font-family: arial;
-                font-size: 14px;
-            }
-            div#interface{
-                background-color: white;
-                width: 1250px;
-                margin: 0px auto 10px auto;
-                box-shadow: 0px 0px 10px;
-                padding: 0px 30px 50px 30px;
-            }
-            img {
-                margin: 0 auto;
-                text-align: center;
-            }
-            .btn-insira:hover {
-                background-color:#F35548; /* Green */
-                color: white;
-
-            }
-            button.btn-insira{
-
-                color: #2E2E2E;
-                border: 2px solid #A4A4A4;
-                cursor: pointer;
-                border-radius: 5px;
-                padding: 10px;
-                font-size: 15px;
-                margin-top: 10px;
-                margin-left: 50px;
-                margin-bottom: 20px;
-
-            }
-            caption{
-                font-size: 20px;
-                margin-top: 20px;
-                margin-bottom: 20px;
-            }
-
-
-            td{
-                text-align: center;
-            }
-            .voltar{
-                background: white;
-                cursor: pointer;
-            }
-        </style>
-
+body{
+    background-color: #dddddd;
+    color: rgba(0,0,0,1);
+    font-family: arial;
+    font-size: 14px;
+}
+div#interface{
+     background-color: white;
+    width: 1250px;
+    margin: 0px auto 10px auto;
+    box-shadow: 0px 0px 10px;
+    padding: 0px 30px 50px 30px;
+}
+ img {
+	margin: 0 auto;
+	text-align: center;
+}
+ .btn-insira:hover {
+    background-color:#F35548; /* Green */
+    color: white;
+    
+}
+button.btn-insira{
+      
+    color: #2E2E2E;
+    border: 2px solid #A4A4A4;
+    cursor: pointer;
+    border-radius: 5px;
+    padding: 10px;
+    font-size: 15px;
+    margin-top: 10px;
+    margin-left: 50px;
+    margin-bottom: 20px;
+    
+}
+caption{
+    font-size: 20px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+}
+             
+             
+             td{
+                 text-align: center;
+             }
+               .voltar{
+                 background: white;
+                 cursor: pointer;
+             }
+            
 
     </style>
 </head>
 <body>
     <div id="interface">
-
-
-        <?php
-        //session_start();
-        include '../cabecalho.php';
-        include '../bd/conectar.php';
+          <?php
+        // session_start ();
+        include  '../cabecalho.php ' ;
+        include  '../bd/conectar.php ' ;
         ?>
 
-        <form method="post" action="pesquisa.php?a=buscar" class="form-pesquisa">
-            <div class="form_pesquisa">   
+            <form method="post" action="pesquisa.php?a=buscar" class="form-pesquisa">
+                <div class="form_pesquisa">   
 <!--                Pesquisar alunos: <input required="" type="search" placeholder="Por nome" name="pesquisaAluno">-->
-                <input required="" type="text" placeholder="   Pesquisar tipo de curso..." name="pesquisaTipo" />
+                    <input required="" type="text" placeholder="   Pesquisar tipo de curso..." name="pesquisaTipo" />
                 <button><?php include "../img/magnifying-glass.svg"; ?></button>
-            </div>
-        </form>
+                </div>
+                </form>
 
-        <?php
-        ini_set("display_errors", true);
+            <?php
+            ini_set("display_errors", true);
 
-        $sql = "select * from tipo order by nome";
+            $sql = "select * from tipo order by nome";
 
-        $resultado = mysqli_query($conexao, $sql);
-        ?>
+            $resultado = mysqli_query($conexao, $sql);
+            ?>
 
-        <form action="excluir_lote.php" method="post">    
-            
-            <table id="customers">
+            <form action="excluir_lote.php" method="post">    
+                <table id="customers">
                 <caption>Tipos de Curso Cadastrados</caption>
-                <tr class="estilo">
-                    <td>Selecionar</td><td>Nome</td><td>Descrição</td><td>Excluir</td><td>Alterar</td>
-                </tr>
-                <?php
-                while ($linha = mysqli_fetch_array($resultado)) {
-                    ?>
-                    <tr>
-                        <td><input type="checkbox" name="id[]" value="<?= $linha['id'] ?>"</td>
-                        <td><?= $linha['nome'] ?></td>
-                        <td><?= $linha['descricao'] ?></td>
-                        <td><a href="excluir.php?id=<?= $linha['id'] ?>">
-                                <img src="../img/excluir2.png" height="30" width="30"/></a></td>
+                
 
-                        <td><a href="form_alterar.php?id=<?= $linha['id'] ?>">
-                                <img src="../img/alterar2.png" height="30" width="30"/></a></td>
+                    <tr class="estilo">
+                        <td>Selecionar</td><td>Nome</td><td>Descrição</td><td>Excluir</td><td>Alterar</td>
                     </tr>
                     <?php
-                }
-                ?>
+                    while ($linha = mysqli_fetch_array($resultado)) {
+                        ?>
+                        <tr>
+                            <td><input type="checkbox" name="id[]" value="<?= $linha['id'] ?>"</td>
+                            <td><?= $linha['nome'] ?></td>
+                            <td><?= $linha['descricao'] ?></td>
+                            <td><a href="excluir.php?id=<?= $linha['id'] ?>">
+                                    <img src="../img/excluir2.png" height="30" width="30"/></a></td>
 
-            </table>
-            <button class="btn-insira">Excluir</button>
+                            <td><a href="form_alterar.php?id=<?= $linha['id'] ?>">
+                                    <img src="../img/alterar2.png" height="30" width="30"/></a></td>
+                        </tr>
+                        <?php
+                    }
+                    ?>
 
-        </form>
+                </table>
+                <button class="btn-insira">Excluir</button>
 
-    </div>
+            </form>
 
+        </div>
+
+       
     <?php
     require_once '../rodape.php';
     ?>
+
