1c2b07280e2f3ddd95db79a185a6dffedcfd18fa
Iago-sc <42946382+Iago-sc@users.noreply.github.com>
2018-11-05 23:00:06 -0200

    Update pesquisa.php

--- SistemaAcademico/turma/pesquisa.php
+++ SistemaAcademico/turma/pesquisa.php
@@ -5,39 +5,139 @@
         <meta charset="utf-8">
         <!--        <link href="../css/estilo.css" rel="stylesheet">-->
         <style>
-            #customers {
-                font-family: Arial;
-                border-collapse: separate;
-                width: 100%;
-            }
+         #customers {
+    font-family: arial;
+    border-collapse: separate;
+     border-spacing: 1px;
+    width: 100%;
+    margin-top: 80px;
+    color:threeddarkshadow;
+}
 
-            #customers td, #customers th {
-                border: 1px solid #ddd;
-                padding: 2.5px;
-            }
+#customers td, #customers th {
+    border: 1px solid #ddd;
+    padding: 2.5px;
+}
 
-            #customers tr:nth-child(even){background-color: #f2f2f2;}
+#customers tr.estilo{
+    background-color: #ccc;
+    color: black;
+}
 
-            #customers tr:hover {background-color: #ddd;}
 
-            #customers th {
-                padding-top: 12px;
-                padding-bottom: 12px;
-                text-align: left;
-                background-color: #4CAF50;
-                color: white;
-            }
-            .btn{
-                font-family: arial;
-                font-size: 14px;
-                border: none;
-                padding: 10px;
-                cursor: pointer;
-                background-color:green;
-                margin-top: 10px;
-            }
-            button.btn-insira{
-      
+
+#customers tr:nth-child(even){background-color: #f2f2f2;}
+
+
+
+#customers tr:hover {background-color: #ddd;
+ color:  black;
+}
+
+#customers th {
+    padding-top: 12px;
+    padding-bottom: 12px;
+    text-align: left;
+    background-color: #4CAF50;
+    color: white;
+}
+.btn-excluir{
+    font-family: arial;
+    font-size: 14px;
+    border-radius: 10px;
+    padding: 10px;
+    cursor: pointer;
+    background-color:red;
+    margin-top: 10px;
+   
+}
+
+body{
+    background-color: #dddddd;
+    color: rgba(0,0,0,1);
+    font-family: arial;
+    font-size: 14px;
+}
+div#interface{
+    background-color: white;
+    width: 1250px;
+    margin: 0px auto 10px auto;
+    box-shadow: 0px 0px 10px;
+    padding: 0px 30px 50px 30px;
+}
+
+.form-pesquisa{
+    position: absolute;
+    left: 900px;
+   margin-bottom: 100px;
+   
+}
+ .btn-insira:hover {
+    background-color:#FE2E2E; /* Green */
+    color: white;
+    
+    
+}
+button.btn-insira{
+    color: #2E2E2E;
+    border: 2px solid #A4A4A4;
+    cursor: pointer;
+    border-radius: 5px;
+    padding: 10px;
+    font-size: 15px;
+    margin-top: 15px;
+    margin-left: 25px;
+    margin-bottom: 20px;
+}
+caption{
+    font-size: 20px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+}
+
+             form{
+                 background-color: white;
+                 margin-top: -20px;
+                 margin-left: 20px;
+                 margin-right: 20px;
+             }
+                h3{
+            display: flex;
+            justify-content: center;
+              
+          }
+         .btn-continuar:hover {
+    background-color:green; /* Green */
+    color: white;
+    
+}
+.button{
+    color: #2E2E2E;
+    border: 2px solid #A4A4A4;
+    cursor: pointer;
+    border-radius: 5px;
+    padding: 10px;
+    font-size: 15px;
+    
+}
+          
+          .btn-continuar{
+              position: absolute;
+              left: 590px;
+             
+          }
+
+.btn-insira:hover {
+    background-color:#F35548; /* Green */
+    color: white;
+    
+}
+
+
+td{
+    text-align: center;
+}
+button.btn-insira{
     color: #2E2E2E;
     border: 2px solid #A4A4A4;
     cursor: pointer;
@@ -45,11 +145,10 @@
     padding: 10px;
     font-size: 15px;
     margin-top: 10px;
-    margin-left: 50px;
+    margin-left: 5px;
     margin-bottom: 20px;
     
-}   
-
+}
 
         </style>
 
@@ -80,23 +179,23 @@
                     <form action="excluir_lote.php" method="post">
                         <table id="customers">
                             <caption>Turmas Cadastradas</caption>
-                            <tr>
-                                <td align="center" valign="top">Selecionar</td><td align="center" valign="top">Número de vagas</td><td align="center" valign="top">Disciplina</td><td align="center" valign="top">Semestre</td><td align="center" valign="top">Professor(a)</td><td align="center" valign="top">Excluir</td><td align="center" valign="top">Alterar</td>
+                            <tr class="estilo">
+                                <td>Selecionar</td><td>Número de vagas</td><td>Disciplina</td><td>Semestre</td><td>Professor(a)</td><td>Excluir</td><td>Alterar</td>
                             </tr>
                             <?php
                             while ($linha = mysqli_fetch_array($resultado)) {
                                 ?>
                                 <tr>
-                                    <td align="center" valign="top"><input type="checkbox" name="id[]" value="<?= $linha['id'] ?>"</td>
-                                    <td align="center" valign="top"><?= $linha['nVagas'] ?></td>
-                                    <td align="center" valign="top"><a href="../disciplina/listar.php"><?= $linha['nome'] ?></a></td>
-                                    <td align="center" valign="top"><?= $linha['valor'] ?></td>
-                                    <td align="center" valign="top"><?= $linha['professor_nome'] ?></td>
+                                    <td><input type="checkbox" name="id[]" value="<?= $linha['id'] ?>"</td>
+                                    <td><?= $linha['nVagas'] ?></td>
+                                    <td><a class="disciplina" href="../disciplina/listar.php"><?= $linha['nome'] ?></a></td>
+                                    <td><?= $linha['valor'] ?></td>
+                                    <td><?= $linha['professor_nome'] ?></td>
 
-                                    <td align="center" valign="top"><a href="excluir.php?id=<?= $linha['id'] ?>">
+                                    <td><a href="excluir.php?id=<?= $linha['id'] ?>">
                                             <img src="../img/excluir2.png" height="30" width="30"/></a></td>
 
-                                    <td align="center" valign="top"><a href="form_alterar.php?id=<?= $linha['id'] ?>">
+                                    <td><a href="form_alterar.php?id=<?= $linha['id'] ?>">
                                             <img src="../img/alterar2.png" height="30" width="30"/></a></td>
                                 </tr>
                                 <?php
@@ -121,4 +220,5 @@
         }
         require_once '../rodape.php';
         ?>
+    
     
