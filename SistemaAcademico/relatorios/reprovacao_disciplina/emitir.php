440c63b746191d736206894330cf6558257866b6
iago-sc <Usuario@Acer>
2018-11-16 13:20:38 -0200

    Correção do relatório de reprovação por disciplina

--- SistemaAcademico/relatorios/reprovacao_disciplina/emitir.php
+++ SistemaAcademico/relatorios/reprovacao_disciplina/emitir.php
@@ -103,7 +103,11 @@
         $curso_id = $_GET["curso"];
         $turma_id = $_GET["turma_id"];
         
-         $sql = "select nota.aluno_id, sum(nota)/count(nota) as media from nota where turma_id=$turma_id group by aluno_id order by aluno_id";
+         $sql = "select distinct aluno.id, aluno.nome, aluno_curso.matricula, sum(nota)/count(nota) as media "
+                    . "from aluno join aluno_curso on aluno.id=aluno_curso.aluno_id join aluno_turma on "
+                    . "aluno_turma.aluno_id=aluno_curso.aluno_id join nota on nota.aluno_id=aluno_turma.aluno_id where "
+                    . "aluno_curso.curso_id=$curso_id AND aluno_turma.turma_id=$turma_id and nota.turma_id=$turma_id group by "
+                    . "nota.aluno_id order by aluno.nome";
         $sql_curso = "select nome from curso where id=$curso_id";
       $sql_turma = "select disciplina.nome from turma join disciplina on disciplina.id=turma.disciplina_id where turma.id=$turma_id";
       
@@ -113,10 +117,13 @@
         $resultado_curso = mysqli_query($conexao, $sql_curso);
         $resultado_turma = mysqli_query($conexao, $sql_turma);
         
+         
+        
+        
         $reprovados=0;
         $linha_curso = mysqli_fetch_array($resultado_curso);
        $linha_turma = mysqli_fetch_array($resultado_turma);
-        
+       
         
         
       while (($linha = mysqli_fetch_array($resultado))){
@@ -124,8 +131,28 @@
         if($linha['media']<6){
             $reprovados = $reprovados+1;
         }
-        
-        }?>
+         
+                        $sql_qtdChamadas = "select disciplina.nome, count(frequencia.frequencia) as qtdChamadas from frequencia join "
+                                . "turma on turma.id=frequencia.turma_id join disciplina on disciplina.id=turma.disciplina_id where "
+                                . "frequencia.aluno_id=$linha[id] and disciplina.nome='$linha_turma[nome]'";
+                        $resultado_qtdChamadas = mysqli_query($conexao, $sql_qtdChamadas);
+                        $linha_qtdChamadas = mysqli_fetch_array($resultado_qtdChamadas);
+                            
+                        $sql_qtdPresencas = "select disciplina.nome, count(frequencia.frequencia) as qtdPresencas from frequencia join "
+                                . "turma on turma.id=frequencia.turma_id join disciplina on disciplina.id=turma.disciplina_id where "
+                                . "(frequencia.aluno_id=$linha[id] and disciplina.nome='$linha_turma[nome]') and frequencia.frequencia='presenca'";
+                        $resultado_qtdPresencas = mysqli_query($conexao, $sql_qtdPresencas);
+                        $linha_qtdPresencas = mysqli_fetch_array($resultado_qtdPresencas);
+                        $frequencia = ($linha_qtdPresencas['qtdPresencas'] / $linha_qtdChamadas['qtdChamadas']) * 100;
+                        
+                       
+                    
+         if($frequencia<75){
+            $reprovados = $reprovados+1;
+        }               
+                        
+      }
+                        ?>
         
          <table id="customers">
              <caption>Relatório de reprovação por disciplina</caption>
